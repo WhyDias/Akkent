9d43b675bf240f20e9a2c7d2eab54a0e28232ac3
WhyDias <diasmaidanbek@gmail.com>
2021-01-27 11:32:04 +0600

    third commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,44 @@
-$(document).ready(function() {
-    $('.slider').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        responsive: [
-            {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
-            }
-            },
-            {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-            }
-    ]}
-    );
-})
+jQuery(document).ready(function($){
+
+$('.button-for-a1').on({
+     'click': function(){
+         $('#change-image').attr('src','A1.jpg');
+     }
+});
+ 
+$('.button-for-a2').on({
+     'click': function(){
+         $('#change-image').attr('src','A2.jpg');
+     }
+});
+ 
+$('.button-for-a3').on({
+     'click': function(){
+         $('#change-image').attr('src','A3.jpg');
+     }
+});
+ 
+$('.button-for-a4').on({
+     'click': function(){
+         $('#change-image').attr('src','A4.jpg');
+     }
+});
+
+$('.button-for-a5').on({
+    'click': function(){
+        $('#change-image').attr('src','A5.jpg');
+    }
+});
+
+$('.button-for-a6').on({
+    'click': function(){
+        $('#change-image').attr('src','A6.jpg');
+    }
+});
+
+$('.button-for-a7').on({
+    'click': function(){
+        $('#change-image').attr('src','A7.jpg');
+    }
+});
+});
