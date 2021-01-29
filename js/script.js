cf34db3b65a020abe2d9badb9d87054227988fba
WhyDias <diasmaidanbek@gmail.com>
2021-01-29 09:45:44 +0600

    change folders and some css

--- js/script.js
+++ js/script.js
@@ -2,50 +2,50 @@ jQuery(document).ready(function($){
 
 $('.button-for-a1').on({
      'click': function(){
-         $('#change-app-plan').attr('src','A1.jpg');
-         $('#change-floor').attr('src','A1-floor.jpeg');
+         $('#change-app-plan').attr('src','resources/A1.jpg');
+         $('#change-floor').attr('src','resources/A1-floor.jpeg');
      }
 });
  
 $('.button-for-a2').on({
      'click': function(){
-         $('#change-app-plan').attr('src','A2.jpg');
-         $('#change-floor').attr('src','A2-floor.jpeg');
+         $('#change-app-plan').attr('src','resources/A2.jpg');
+         $('#change-floor').attr('src','resources/A2-floor.jpeg');
      }
 });
  
 $('.button-for-a3').on({
      'click': function(){
-         $('#change-app-plan').attr('src','A3.jpg');
-         $('#change-floor').attr('src','A3-floor.jpeg');
+         $('#change-app-plan').attr('src','resources/A3.jpg');
+         $('#change-floor').attr('src','resources/A3-floor.jpeg');
      }
 });
  
 $('.button-for-a4').on({
      'click': function(){
-         $('#change-app-plan').attr('src','A4.jpg');
-         $('#change-floor').attr('src','A4-floor.jpeg');
+         $('#change-app-plan').attr('src','resources/A4.jpg');
+         $('#change-floor').attr('src','resources/A4-floor.jpeg');
      }
 });
 
 $('.button-for-a5').on({
     'click': function(){
-        $('#change-app-plan').attr('src','A5.jpg');
-        $('#change-floor').attr('src','A5-floor.jpeg');
+        $('#change-app-plan').attr('src','resources/A5.jpg');
+        $('#change-floor').attr('src','resources/A5-floor.jpeg');
     }
 });
 
 $('.button-for-a6').on({
     'click': function(){
-        $('#change-app-plan').attr('src','A6.jpg');
-        $('#change-floor').attr('src','A6-floor.jpeg');
+        $('#change-app-plan').attr('src','resources/A6.jpg');
+        $('#change-floor').attr('src','resources/A6-floor.jpeg');
     }
 });
 
 $('.button-for-a7').on({
     'click': function(){
-        $('#change-app-plan').attr('src','A7.jpg');
-        $('#change-floor').attr('src','A7-floor.jpeg');
+        $('#change-app-plan').attr('src','resources/A7.jpg');
+        $('#change-floor').attr('src','resources/A7-floor.jpeg');
     }
 });
 });
